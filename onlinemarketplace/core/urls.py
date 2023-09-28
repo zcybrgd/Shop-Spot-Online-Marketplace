3a5162c869924f1a693c61d348548169684d7324
zed <zainebb7k9h1@gmail.com>
2023-09-28 23:37:16 +0100

    linking problem

--- onlinemarketplace/core/urls.py
+++ onlinemarketplace/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 app_name = 'core'
-url_patterns = [
+urlpatterns = [
  path('', views.index, name='index'),
  path('contact/', views.contact, name='contact'),
- path('signup/', views.signup, name='signup')
+ path('signup/', views.signup, name='signup'),
 ]
