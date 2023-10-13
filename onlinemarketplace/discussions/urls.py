3d7cb0604009227a30cfda47d609c096f7333f92
zed <zainebb7k9h1@gmail.com>
2023-10-13 21:04:59 +0100

    --

--- onlinemarketplace/discussions/urls.py
+++ onlinemarketplace/discussions/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name='discussions'
 
 urlpatterns = [
+    path('',views.inbox, name='inbox'),
+    path('<int:pk>', views.inside_convo, name='inside'),
     path('new/<int:item_pk>/', views.new_convo, name='new_convo'),
 
 ]
--- onlinemarketplace/discussions/views.py
+++ onlinemarketplace/discussions/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from items.models import Item
 from .models import Discussion, Conversation
 from .forms import ConversationMessageForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def new_convo(request, item_pk):
     item = get_object_or_404(Item, pk=item_pk)
     if item.created_by == request.user:
@@ -10,7 +13,7 @@ def new_convo(request, item_pk):
     discussions = Discussion.objects.filter(item=item).filter(engaging_users__in=[request.user.id])
 
     if discussions:
-        pass # redirect l kesh plassa
+        return redirect('discussions:inside',pk=discussions.first().id)
 
     if request.method == 'POST':
         form = ConversationMessageForm(request.POST)
@@ -28,5 +31,37 @@ def new_convo(request, item_pk):
         form = ConversationMessageForm()
 
     return render(request, 'discussions/new.html', {
+        'form' : form
+    })
+
+@login_required
+def inbox(request):
+    # get all the discussions that the user is in
+    discussions = Discussion.objects.filter(engaging_users__in=[request.user.id])
+    return render(request, 'discussions/inbox.html', {
+        'discussions':discussions
+    })
+
+@login_required
+def inside_convo(request,pk):
+    discussion = Discussion.objects.filter(engaging_users__in=[request.user.id]).get(pk=pk)
+
+    if request.method == 'POST':
+        form = ConversationMessageForm(request.POST)
+
+        if form.is_valid():
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = discussion
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            discussion.save()
+
+            return redirect('conversation:detail', pk=pk)
+    else:
+        form = ConversationMessageForm()
+
+    return render(request,'discussions/inside.html',{
+        'discussion' : discussion,
         'form' : form
     })
